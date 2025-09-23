56724fbaa58e276a272a9b1e8a547a98d6c40de2
akaro910-crypto <akaro910@gmail.com>
2025-09-23 22:32:37 +0000

    promedio nota con porcentajes

---
+++ 05_app.py
@@ -0,0 +1,27 @@
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+# Inicio: Definir variables de las notas
+nota_t1 = 16
+nota_t2 = 10
+nota_ef = 13
+
+# Proceso: Definir el proceso de cálculo del promedio de notas
+promedio_notas = (nota_t1 + nota_t2 + nota_ef) / 3
+
+# Salida: Mostrar el resultado del promedio
+print(f"promedio de robert : {promedio_notas}")
+
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+# Inicio: Definir variables de las notas
+nota_t1 = 16
+nota_t2 = 10
+nota_ef = 13
+
+# Proceso: Definir el proceso de cálculo del promedio de notas
+# T1 (20%) + T2 (35%) + EF (45%)
+promedio_nota = (nota_t1 * .2 + nota_t2 * .35 + nota_ef * .45)
+
+# Salida: Mostrar el resultado del promedio
+
+print(f"promedio final robert : {promedio_nota}")
