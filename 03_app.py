c189696d4037269842ad75b5ee79860a6d79396e
akaro910-crypto <akaro910@gmail.com>
2025-09-23 20:31:43 +0000

    empezamos con variables

---
+++ 03_app.py
@@ -0,0 +1,13 @@
+# variables
+
+# las variables son palabras especificas que utilizamos para asignar y almacenar valores especificos que podemos recuperar en cualquier momento.
+
+# variable nombre
+nombre = "robert"
+telefono = 987654321
+name = "joel"
+
+# vizualizando el contenido de las variables
+print(nombre)
+print(telefono)
+print(name)
