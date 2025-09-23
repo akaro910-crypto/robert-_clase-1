30158dfba9835f26ab32f96a3f3f3863c3d47756
akaro910-crypto <akaro910@gmail.com>
2025-09-23 22:46:56 +0000

    variables str computador, int numero entero y float numero decimal. decimal con comillas str

---
+++ 06_app.py
@@ -0,0 +1,17 @@
+# Averiguando los tipos de datos de las variables
+
+# Variable de tipo cadena de texto
+dispositivo = "Computador"
+print(dispositivo)
+print(type(dispositivo))
+
+# Variable de tipo entero
+cantidad = 45
+print(cantidad)
+print(type(cantidad))
+
+
+# Variable de tipo decimal / flotante
+precio = "14.99"
+print(precio)
+print(type(precio))
