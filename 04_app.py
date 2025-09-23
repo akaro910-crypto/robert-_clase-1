a48d2ee691119e973f2022f6d7e90e2d41928969
akaro910-crypto <akaro910@gmail.com>
2025-09-23 21:50:10 +0000

    operaciones de calculo con igv

--- 04_app.py
+++ 04_app.py
@@ -8,6 +8,9 @@ igv = 0.18
 monto_a_pagar = monto_pago + (monto_pago * igv)
 
 # Salida: Visualización de resultados
+print(monto_a_pagar)
 print(f"El monto es: {monto_pago}")
 print(f"El IGV es: {igv}")
 print(f"El monto a pagar es: {monto_a_pagar}")
+igv_total = (monto_pago * igv)
+print(f"el 18% es : {igv_total}")
