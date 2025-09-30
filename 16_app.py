9b154c7e29188fa32654bbde607832d3c7d98a3e
akaro910-crypto <akaro910@gmail.com>
2025-09-30 02:38:08 +0000

    ejemplos con bucle for

---
+++ 16_app.py
@@ -0,0 +1,19 @@
+# Estructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de frutas
+frutas = ["manzana", "mandarina", "arándanos", "fresas", "maracuyá"]
+
+for fruta in frutas:
+    print(fruta)
+
+# listado de carros
+carros = ["toyota", "nissan", "chevrolec", "audi", "kia"]
+
+for carro in carros:
+    print(carro)
